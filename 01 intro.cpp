758e0519159f648b65e07e66e5d071c1ddf46190
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-03-04 23:22:26 +0530

    Update 01 intro.cpp

--- 01 intro.cpp
+++ 01 intro.cpp
@@ -2,7 +2,23 @@ used for FIXED size arrays
 
 supports 2 types of operations:
 1. range query....do something in a range...in O(log N) time
-2. update a value....O(log N)
+2. update a value.... O(log N)
 
 Preprocessing time O(N)
 Preprocessing space O(N)
+
+  
+  
+Example:
+given an array-> do 2 operations-> 1. find sum on range on index, and 2. update value at an index
+
+Naive sol:
+find sum in O(N)
+update in O(1)
+  
+Prefix sum approach:
+find sum in O(1)
+update in O(N) => because we need to update whole prefix array
+
+
+BEST sol=> segment tree
