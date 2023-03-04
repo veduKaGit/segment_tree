dff2f1b26207ba17d1d4be04a9d0feaeaebdd5f2
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-03-04 23:16:47 +0530

    Create 01 intro.cpp

---
+++ 01 intro.cpp
@@ -0,0 +1,8 @@
+used for FIXED size arrays
+
+supports 2 types of operations:
+1. range query....do something in a range...in O(log N) time
+2. update a value....O(log N)
+
+Preprocessing time O(N)
+Preprocessing space O(N)
