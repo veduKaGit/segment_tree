8cc9a9191edf74b1cf5aba14f825161b345ad99a
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-03-05 20:13:10 +0530

    Update 03 construction.cpp

--- 03 construction.cpp
+++ 03 construction.cpp
@@ -14,9 +14,9 @@ int CST(int ss, int se, int si, int arr[], int tree[])
 	int mid = (ss + se) / 2;
 
 	tree[si] = CST(ss, mid, 2*si+1, arr, tree) + CST(mid + 1, se, 2*si+2, arr, tree);  //for [ss,mid] -> 2*si+1, and [mid+1,se] -> 2*si+2
-	//why are we adding both? -> since in this example, we are considering sum in the range [ss,se]
+	//why are we adding both? -> since in this example, we are considering SUM in the range-> thus total sum = sum of left and right halves
 	
-	//also notice, since we are assigning value to tree[si], thus some nodes in tree array may NOT be assigned a value(dummy nodes)
+	//also notice, since we are assigning value to tree[si], thus some nodes in tree array may NOT be assigned a value(dummy nodes) -> see 02_imp.png
 	
 	return tree[si]; //return tree[si] -> since in this example, we are considering sum in the range [ss,se]
 }	
